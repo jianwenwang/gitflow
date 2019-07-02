cafa373727beb34faaa31849d73806c5c2675751
汪建稳 <13647285420@139.com>
2019-07-02 15:26:16 +0800

    添加获取单个用户接口

--- trunk/runserver.py
+++ trunk/runserver.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return json.dumps({'code':10,'data':[],'msg':'hello world!'})
 
+@app.route('/getUser')
+def get_user():
+    return json.dumps({'id':1,'name':'耿达达'})
 	
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
