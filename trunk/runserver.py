a8c0a1c64a6ab338b3110aa7f6c91060ab798a28
汪建稳 <13647285420@139.com>
2019-07-02 15:28:26 +0800

    添加删除用户信息接口

--- trunk/runserver.py
+++ trunk/runserver.py
@@ -12,6 +12,9 @@ def hello_world():
 def query_user():
     return json.dumps([{'id':1,'name':'沈航宇'},{'id':2,'name':'耿达达'}])
 
-	
+@app.route('/deleteUser')
+def delete_user()
+    return json.dumps({'code':10,'data':'','msg':'ok'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
