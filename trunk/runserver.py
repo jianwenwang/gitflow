42e005cb444700ed4afe921476f3387eac5a9a5a
汪建稳 <13647285420@139.com>
2019-07-02 15:22:03 +0800

    添加查询列表接口

--- trunk/runserver.py
+++ trunk/runserver.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return json.dumps({'code':10,'data':[],'msg':'hello world!'})
+	
+
+@app.route('/queryUser')
+def query_user():
+    return json.dumps([])
 
 	
 if __name__ == '__main__':
