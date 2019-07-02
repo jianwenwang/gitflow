ccf82c6aa6cd29b5ce5e5a0eef4483978de01d46
汪建稳 <13647285420@139.com>
2019-07-02 15:35:10 +0800

    dev-功能测试1 merge

--- trunk/runserver.py
+++ trunk/runserver.py
@@ -11,5 +11,13 @@ def hello_world():
 def get_user():
     return json.dumps({'id':1,'name':'耿达达'})
 	
+@app.route('/queryUser')
+def query_user():
+    return json.dumps([{'id':1,'name':'沈航宇'},{'id':2,'name':'耿达达'}])
+
+@app.route('/deleteUser')
+def delete_user()
+    return json.dumps({'code':10,'data':'','msg':'ok'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
