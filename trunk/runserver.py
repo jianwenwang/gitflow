8c96d5ce4b5c233f89857fcc4610ae22f894fadb
汪建稳 <13647285420@139.com>
2019-07-03 15:00:22 +0800

    这里写修改说明

--- trunk/runserver.py
+++ trunk/runserver.py
@@ -9,24 +9,20 @@ def hello_world():
 
 @app.route('/getUser')
 def get_user():
-    return json.dumps({'id':1,'name':'耿达达'})
+    return json.dumps({'id':1,'name':'gdd'})
 	
 @app.route('/queryUser')
 def query_user():
-    return json.dumps([{'id':1,'name':'沈航宇'},{'id':2,'name':'耿达达'}])
+    return json.dumps([{'id':1,'name':'shy'},{'id':2,'name':'gdd'}])
 
 @app.route('/deleteUser')
-def delete_user()
+def delete_user():
     return json.dumps({'code':10,'data':'','msg':'ok'})
 	
 @app.route('/')
-def change_mobile()
-    print('change mobile')
+def change_mobile():
     return json.dumps({'code':10,'data':'','msg':'ok'})
-	print('changed succeed!')
 
 if __name__ == '__main__':
-    print('start run!')
-    app.run('0.0.0.0', 5000, debug=True)
-	print('start succeed!')
+    app.run('0.0.0.0', 5000, debug=True) 
 
